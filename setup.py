6d4656142451d1bad8004cbf30b08fce74279b71
sarihsasra <116100818+sarihsasra@users.noreply.github.com>
2022-10-18 23:04:45 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "jewer.py",
+    version = "0.1",
+    py_modules = ['jewer'],
+    url = "https://github.com/indraxz/jewer.git",
+    author = "indraxz",
+    install_requires = ["pyngrok"]
+)
